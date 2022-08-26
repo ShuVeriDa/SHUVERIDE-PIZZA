927709c402b996e1c74e3719f679cad88ab9cac6
ShuVeriDA <bashtarov@outlook.com>
2022-08-26 12:32:37 +0300

    added component Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import './scss/app.scss';
+import {Header} from "./components/Header";
+import {Categories} from "./components/Categories";
+
 
 function App() {
-  return (
-    <div className="App">
+   return (
+      <div className="wrapper">
+         <Header/>
+         <div className="content">
+            <div className="container">
+               <div className="content__top">
+                  <Categories/>
 
-    </div>
-  );
+               </div>
+               <h2 className="content__title">Все пиццы</h2>
+               <div className="content__items">
+                  {/*<% include components/pizza-block.ejs %> <% include components/pizza-block.ejs %> <%*/}
+                  {/*include components/pizza-block.ejs %> <% include components/pizza-block.ejs %> <%*/}
+                  {/*include components/pizza-block.ejs %> <% include components/pizza-block.ejs %> <%*/}
+                  {/*include components/pizza-block.ejs %> <% include components/pizza-block.ejs %> <%*/}
+                  {/*include components/pizza-block.ejs %>*/}
+               </div>
+            </div>
+         </div>
+      </div>
+   );
 }
 
 export default App;
