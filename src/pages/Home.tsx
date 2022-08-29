c4047647fb8100a335755481b6fba704c331e059
ShuVeriDA <bashtarov@outlook.com>
2022-08-29 19:57:25 +0300

    added static search pizzas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,42 @@
-import React, {useEffect, useState} from "react";
+import React, {FC, useEffect, useState} from "react";
 import {Categories} from "../components/Categories";
 import {Sort} from "../components/Sort";
 import {PizzaBlock} from "../components/PizzaBlock/PizzaBlock";
 import {PizzaType} from "../App";
 import {Skeleton} from "../components/PizzaBlock/Skeleton";
 
+type HomePropsType = {
+   searchValue: string
+   setSearchValue: (searchValue: string) => void
+}
+
 export type SortType = {
    name: string,
    sortProperty: string,
 }
 
-export const Home = () => {
-   const [items, setItems] = useState([])
+export const Home: FC<HomePropsType> = ({searchValue, setSearchValue}) => {
+   const [items, setItems] = useState<PizzaType[]>([])
    const [isLoading, setIsLoading] = useState<boolean>(true)
    const [categoryId, setCategoryId] = useState<number>(0)
    const [sortType, setSortType] = useState<SortType>({
       name: 'популярности',
       sortProperty: 'rating',
    })
+
    const array = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]
+   const skeleton = array.map((_, index) => <Skeleton key={index}/>)
+   const pizzas = items
+      .filter((obj) => {
+         if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+            return true
+         }
+
+         return false
+      })
+      .map((obj) => (
+         <PizzaBlock key={obj.id} {...obj}/>
+      ))
 
    useEffect(() => {
       setIsLoading(true)
@@ -36,6 +54,8 @@ export const Home = () => {
       window.scroll(0, 0)
    }, [categoryId, sortType])
 
+
+
    return (
       <div className='container'>
          <div className="contentTop">
@@ -44,12 +64,7 @@ export const Home = () => {
          </div>
          <h2 className="contentTitle">Все пиццы</h2>
          <div className="contentItems">
-            {isLoading
-               ? array.map((_, index) => <Skeleton key={index}/>)
-               : items.map((obj: PizzaType) => (
-                  <PizzaBlock key={obj.id} {...obj}/>
-               ))
-            }
+            {isLoading ? skeleton : pizzas}
          </div>
       </div>
    );
