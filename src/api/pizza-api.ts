1402d314ec19eee6d2500a6e21fb51393af309d1
ShuVeriDa <bashtarov@outlook.com>
2023-04-30 16:20:49 +0300

    refactored code

--- src/api/pizza-api.ts
+++ src/api/pizza-api.ts
@@ -4,7 +4,7 @@ import {PizzaType} from "../redux/pizza/pizzaTypes";
 
 const instance = axios.create({
    baseURL: process.env.REACT_APP_API_URL
-     || 'https://630a32f93249910032824d12.mockapi.io/'
+     || 'https://630a32f93249910032824d12.mockapi.io/items'
 })
 
 
@@ -12,7 +12,7 @@ const instance = axios.create({
 //api
 export const pizzasAPI = {
    getPizzas: (params: SearchPizzasParamsType) => {
-      return instance.get<PizzaType[]>(`items?page=${params.currentPage}&limit=4&${params.category}&sortBy=${params.sortBy}&order=${params.order}&${params.search}`)
+      return instance.get<PizzaType[]>(`?page=${params.currentPage}&limit=4&${params.category}&sortBy=${params.sortBy}&order=${params.order}&${params.search}`)
    },
    // getSelectedPizza: (id: string) => {
    //    return instance.get(`items/${id}`, {})
